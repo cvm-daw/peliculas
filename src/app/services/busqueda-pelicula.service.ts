4c3e326a1b9abeac3c5af95f42682b03f26bca0f
cvm-daw <carlosvalls89@gmail.com>
2021-09-04 14:37:13 +0200

    Modificacion proxy

--- src/app/services/busqueda-pelicula.service.ts
+++ src/app/services/busqueda-pelicula.service.ts
@@ -10,7 +10,7 @@ export class BusquedaPeliculaService {
   constructor(private http: HttpClient) { }
 
   url: string = "http://www.omdbapi.com/?apikey=d3530b33&t=";
-  url2: string = "https://api.themoviedb.org/3/movie/550?api_key=c8ad22799cf0a634f077087d1626d7aa";
+
 
   getPelicula(titulo: string): Observable<any[]>{
     return this.http.get<any[]>(this.url+titulo);
